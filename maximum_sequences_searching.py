25fdbb57359ab4390b5517ea5b99cb787aab818d
Aref.Artem <39958544+arefka@users.noreply.github.com>
2020-08-03 01:19:08 +0500

    refactor: create a function of estimating elements

--- maximum_sequences_searching.py
+++ maximum_sequences_searching.py
@@ -3,26 +3,33 @@
 
     The task is about searching the maximum sequence of elements of monotonically increasing sequence.
 
+    We should split the input sequence to the list of monotonically increasing sequences and
+    return longest one. We can think about some sequence as monotonically increasing
+    if its previous element is smaller then next.
+
 ===============================================================
 '''
-
-some_example_sequence = [1, 5, 2, 7, 8, 4, 6]
 
 class MonotonicallyIncreadingSequence:
     def __init__(self, input_numerable_list: list):
         self.__numerable_list = input_numerable_list
 
-    def __two_adjacent_elements_estimate(self):
-        # TO DO
-        # estimate two left elements and add result to the list
-        return []
+    def __elements_estimate(self):
+        locally_the_best_sequence = [self.__numerable_list.pop(0)]
+        while self.__numerable_list and locally_the_best_sequence[-1:][0] <= self.__numerable_list[0]:
+            locally_the_best_sequence.append(self.__numerable_list.pop(0))
+        return locally_the_best_sequence
 
     def search_the_maximum_sequence(self):
-        # we should split the input sequence to the list of monotonically increasing sequences
         maximum_monotonically_increasing_sequence = []
         while self.__numerable_list:
-            current_monotonically_increasing_sequence = self.search_the_maximum_sequence()
-            if len(maximum_monotonically_increasing_sequence) > len(current_monotonically_increasing_sequence):
+            current_monotonically_increasing_sequence = self.__elements_estimate()
+            if len(maximum_monotonically_increasing_sequence) < len(current_monotonically_increasing_sequence):
                 maximum_monotonically_increasing_sequence = current_monotonically_increasing_sequence
         return maximum_monotonically_increasing_sequence
 
+################# example: #################
+
+example_sequence = [1, 5, 2, 7, 8, 4, 6]
+
+print(MonotonicallyIncreadingSequence(example_sequence).search_the_maximum_sequence())
